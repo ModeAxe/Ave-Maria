5dfd1bbe627aac0a032332543e2035f593b8b4cb
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-05-14 23:09:26 -0400

    scene + camera setup complete

---
+++ Assets/sceneController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneController : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    public GameObject[] cameras;
+    private int currentCam;
+	void Start()
+	{
+		currentCam = 0;
+		setCam(currentCam);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+			toggleCam();
+        }
+
+	}
+
+	public void setCam(int idx)
+	{
+		for (int i = 0; i < cameras.Length; i++)
+		{
+			if (i == idx)
+			{
+				cameras[i].SetActive(true);
+			}
+			else
+			{
+				cameras[i].SetActive(false);
+			}
+		}
+	}
+
+	public void toggleCam()
+	{
+		currentCam++;
+		if (currentCam > cameras.Length - 1)
+			currentCam = 0;
+		setCam(currentCam);
+	}
+}
